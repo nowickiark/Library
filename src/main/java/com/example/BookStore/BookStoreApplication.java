c7321433f3f0b09983bd86aeac7dc43660cb75fd
nowickiark@outlook.com <amrentos103>
2019-08-24 09:02:27 +0200

    Added Command line runner to execute application from command line.

--- src/main/java/com/example/BookStore/BookStoreApplication.java
+++ src/main/java/com/example/BookStore/BookStoreApplication.java
@@ -1,13 +1,28 @@
 package com.example.BookStore;
 
+import com.example.BookStore.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class BookStoreApplication {
+public class BookStoreApplication implements CommandLineRunner {
+
+	private static Logger LOG = LoggerFactory.getLogger(BookStoreApplication.class);
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		orderService.action();
+	}
 }
